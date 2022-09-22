df01fa40decc90b1c2e17cd874c9e73d1a61ee2b
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-09-22 18:49:34 +0500

    Update views.py

--- core/views.py
+++ core/views.py
@@ -26,33 +26,33 @@ def record(request):
 
 def record_detail(request, id):
     record = get_object_or_404(Record, id=id)
-    filename = os.path.join(settings.MEDIA_ROOT, str('new.wav'))
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX : ",filename)
-    out = os.path.join(settings.MEDIA_ROOT, str('out.wav'))
-    data, samplerate = soundfile.read(filename)
-    soundfile.write(out, data, samplerate, subtype='PCM_16')
-    new_file = os.path.join(settings.MEDIA_ROOT, str('out.wav'))
+    
+#     filename = os.path.join(settings.MEDIA_ROOT, str('new.wav'))
+    
+#     out = os.path.join(settings.MEDIA_ROOT, str('out.wav'))
+#     data, samplerate = soundfile.read(filename)
+#     soundfile.write(out, data, samplerate, subtype='PCM_16')
+#     new_file = os.path.join(settings.MEDIA_ROOT, str('out.wav'))
 
-    # print("HHHHHHHHHHHHHHHHHHHH: ",filename)
+    
 
-    file = 'logs.txt'
-    # file_ = open(os.path.join(settings.MEDIA_ROOT, file.file.url))
-    f = open(os.path.join(settings.MEDIA_ROOT, file))
-    print("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP: ",f.readline())
-    f.close()
-    # filename= r"C:\Users\JO\Downloads\Music\No Excuses Keep Practising Your English Listening Ep 565-d0c58c.mp3"
-    r = sr.Recognizer()
+#     file = 'logs.txt'
+#     f = open(os.path.join(settings.MEDIA_ROOT, file))
+#     f.close()
+    
+#     r = sr.Recognizer()
 
-    # open the file
-    try:
-        with sr.AudioFile(new_file) as source:
-            # listen for the data (load audio to memory)
-            audio_data = r.record(source)
-            # recognize (convert from speech to text)
-            text = r.recognize_google(audio_data)
-            print(text)
-    except:
-        text = "not Understand"
+#     # open the file
+#     try:
+#         with sr.AudioFile(new_file) as source:
+#             # listen for the data (load audio to memory)
+#             audio_data = r.record(source)
+#             # recognize (convert from speech to text)
+#             text = r.recognize_google(audio_data)
+#             print(text)
+#     except:
+#         text = "not Understand"
+    text = "not Understand"
 
     context = {
         "page_title": "Recorded audio detail",
