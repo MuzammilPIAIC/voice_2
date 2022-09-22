3a9d406a52137134d809f04d6235dd6ba6b67ce9
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-09-22 16:12:07 +0500

    Update settings.py

--- record/settings.py
+++ record/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.voice-recorder-muzammil.herokuapp.com']
 
